499ce480055f6fc67b141a2746f7f0f11fe85938
Ng Zhun Onn <ngzhunonn@gmail.com>
2021-06-12 03:49:30 +0800

    ✏ Website is now fully responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav__links');
+
+// Display Mobile Menu
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+// Close Mobile Menu when nav links is clicked
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 960 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
